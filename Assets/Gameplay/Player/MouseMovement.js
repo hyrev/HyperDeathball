535be7f16adcf40b7a8d62bb35248b1a9ef46051
ririari <ririari@gmail.com>
2013-11-27 04:04:12 -0400

    Changed the input that controls the paddle
    now the player has to hold the left mouse button (should work better
    with android and iOS devices)

--- Assets/Gameplay/Player/MouseMovement.js
+++ Assets/Gameplay/Player/MouseMovement.js
@@ -1,10 +1,6 @@
 ﻿#pragma strict
 var sensitivityY = 1.0;
 
-function Start () {
-
-}
-
 function Update () {
 	transform.position.y += Input.GetAxis("Mouse Y") * sensitivityY;
 }
---
+++ Assets/Gameplay/Player/TouchMovement.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+var paddle : Transform;
+
+function Update () {
+	if(Input.GetMouseButton(0)){
+		var curScreenPoint = Vector3(paddle.position.x,Input.mousePosition.y,Camera.main.transform.position.z*-1);
+		var curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
+	    transform.position = Vector3(paddle.position.x,curPosition.y,0);
+	}
+}
