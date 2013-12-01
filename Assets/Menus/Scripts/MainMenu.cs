bdc9d9552e3585be2d4de0439b47d49a5b53dd30
Brian <bdinelt@uwo.ca>
2013-12-01 13:26:18 -0500

    Added a Pause Menu
    Added a pause menu. Changed the menu fonts.

--- Assets/Menus/Scripts/MainMenu.cs
+++ Assets/Menus/Scripts/MainMenu.cs
@@ -4,26 +4,25 @@ using System.Collections;
 public class MainMenu : MonoBehaviour {
 
 	public GUISkin guiStyle;
-	public int bezel;
 	
 	void OnGUI() {
 		GUI.skin = guiStyle;
         GUI.BeginGroup(new Rect(0, 0, Screen.width, Screen.height));
-        if(GUI.Button(new Rect(Screen.width * 0.05f, Screen.height * 0.05f, Screen.width * 0.425f, Screen.height * 0.6f), "Single Player")) {
+        if(GUI.Button(new Rect(Screen.width * 0.05f, Screen.height * 0.05f, Screen.width * 0.425f, Screen.height * 0.6f), "SINGLE PLAYER")) {
 			Application.LoadLevel("MainGame");	
 		}
 		
-		if(GUI.Button(new Rect(Screen.width * 0.525f, Screen.height * 0.05f, Screen.width * 0.425f, Screen.height * 0.6f), "Multi-Player")) {
+		if(GUI.Button(new Rect(Screen.width * 0.525f, Screen.height * 0.05f, Screen.width * 0.425f, Screen.height * 0.6f), "MULTI PLAYER")) {
 			//Load multiplayer portion using this format:
 			//Application.LoadLevel("Multiplayer");	
 		}
 		
-		if(GUI.Button(new Rect(Screen.width * 0.05f, Screen.height * 0.7f, Screen.width * 0.425f, Screen.height * 0.25f), "Options")) {
+		if(GUI.Button(new Rect(Screen.width * 0.05f, Screen.height * 0.7f, Screen.width * 0.425f, Screen.height * 0.25f), "OPTIONS")) {
 			//Load the options menu if we get around to it (Difficulty for SP, sound level, etc)
 			//Application.LoadLevel("Option");	
 		}
 		
-		if(GUI.Button(new Rect(Screen.width * 0.525f, Screen.height * 0.7f, Screen.width * 0.425f, Screen.height * 0.25f), "Credits")) {
+		if(GUI.Button(new Rect(Screen.width * 0.525f, Screen.height * 0.7f, Screen.width * 0.425f, Screen.height * 0.25f), "CREDITS")) {
 			//Because we need to be recognised for our hard work
 			//Application.LoadLevel("MainGame");	
 		}
---
+++ Assets/Menus/Scripts/PauseMenu.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+	
+	public GUISkin guiStyle;
+	public Texture pauseButtonImage;
+	public bool isPaused = false;
+	
+	void Update() {
+		//Maybe put stuff here. Probably not
+	}
+	
+	void OnGUI() {
+		GUI.skin = guiStyle;
+        GUI.BeginGroup(new Rect(0, 0, Screen.width, Screen.height));
+		
+		//Pause button
+        if(GUI.Button(new Rect(Screen.width * 0.95f, Screen.height  * 0.05f, 30, 30), pauseButtonImage)) {
+			isPaused = togglePause();	
+		}
+		
+		//Pause Menu
+		if(isPaused) {
+			if(GUI.Button(new Rect(Screen.width * 0.10f, Screen.height * 0.10f, Screen.width * 0.35f, Screen.height * 0.80f), "OPTIONS")) {
+				//Load up the Options menu
+			}
+			
+			if(GUI.Button(new Rect(Screen.width * 0.55f, Screen.height * 0.10f, Screen.width * 0.35f, Screen.height * 0.80f), "MAIN MENU")) {
+				Application.LoadLevel("MainMenu");	
+			}
+		}
+		GUI.EndGroup();
+	}
+	
+	bool togglePause() {
+		if(Time.timeScale == 0f) {
+			Time.timeScale = 1f;
+			return(false);
+		} else {
+			Time.timeScale = 0f;
+			return(true);    
+		}
+	}
+}
