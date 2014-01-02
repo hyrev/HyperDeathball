a5741969446efd26d7f2a42c81144cca608620ee
Jake <jake@Jakes-MacBook-Air.local>
2014-01-02 15:29:30 -0500

    - background work, we now have a cool scrolling starscape instead of just plain blue

---
+++ Assets/Gameplay/Background/Star.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Star : MonoBehaviour
+{
+	public float minScrollSpeed;		//min and max scrolling speed
+	public float maxScrollSpeed;
+	public float minStarScale;			//min and max scale
+	public float maxStarScale;
+
+	private float scrollSpeed;			//actual scroll speed
+	private float starScale;			//actual scale
+
+
+	void Start()
+	{
+		//init the star with random values from within the range
+		regenerateSpeedAndSize();
+		transform.localScale = new Vector3(starScale, starScale, starScale);
+	}
+	
+	void Update()
+	{
+		//move the star across the screen
+		transform.position = new Vector3(transform.position.x + scrollSpeed, transform.position.y, transform.position.z);
+	}
+	
+	public void regenerateSpeedAndSize()
+	{
+		scrollSpeed = Random.Range(minScrollSpeed, maxScrollSpeed);
+		starScale = Random.Range(minStarScale, maxStarScale);
+	}
+}
---
+++ Assets/Gameplay/Background/StarManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarManager : MonoBehaviour
+{
+	public int numberOfStars;		//how many star objects are kept on the screen at one time
+	public int refreshDistance;		//how far fro the manager a star can be before being refreshed
+	public int bgHeight;			//height of the starry area
+	
+	private GameObject[] stars;
+
+	void Start()
+	{
+		//init stars
+		//give them random starting positions across the game area so it doesnt look awful at launch
+		stars = new GameObject[numberOfStars];
+		for(int x = 0; x < numberOfStars; x++)
+		{
+			stars[x] = (GameObject)Instantiate(Resources.Load("prefab_Star"));
+			stars[x].transform.position = new Vector3(transform.position.x + Random.Range(1, refreshDistance), transform.position.y - Random.Range(1, bgHeight), transform.position.z);
+		}
+	}
+	
+	void Update()
+	{
+		//check to see if any stars need to be reloaded, and reload them if they do
+		for(int x = 0; x < numberOfStars; x++)
+		{
+			if(stars[x].transform.position.x - transform.position.x > refreshDistance)
+			{
+				stars[x].GetComponent<Star>().regenerateSpeedAndSize();
+				stars[x].transform.position = new Vector3(transform.position.x, transform.position.y - Random.Range(1, bgHeight), transform.position.z);
+			}
+		}
+	}
+}
