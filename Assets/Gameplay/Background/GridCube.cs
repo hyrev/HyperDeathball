557fad6c5d6ce5e9499b56635733e9e644c04a3c
Jake <jake@Jakes-MacBook-Air.local>
2014-01-02 21:15:32 -0500

    - work-in-progress of the dynamic BG grid - will eventually react to game events like scoring, collisions etc - will also be used to display the current scores via colour/scale changes

---
+++ Assets/Gameplay/Background/GridCube.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridCube : MonoBehaviour
+{
+	public GridCube topNeighbour;
+	public GridCube bottomNeighbour;
+	public GridCube leftNeighbour;
+	public GridCube rightNeighbour;
+
+	void Start()
+	{
+	
+	}
+	
+	void Update()
+	{
+	
+	}
+}
---
+++ Assets/Gameplay/Background/GridManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridManager : MonoBehaviour
+{
+	public int gridWidth;			//how many cubes are in a row
+	public int gridHeight;			//how many cubes are in a column
+	public float cubeXOffset;		//how much space to leave between columns
+	public float cubeYOffset;		//how much space to leave between rows
+	
+	public GameObject[,] grid;		//xy grid of cubes, each with adjacencies
+
+	void Start()
+	{
+		//initialize grid and create GridCubes
+		grid = new GameObject[gridWidth, gridHeight];
+		for(int x = 0; x < gridWidth; x++)
+		{
+			for(int y = 0; y < gridHeight; y++)
+			{
+				//create the objects and place them accordingly
+				grid[x, y] = (GameObject)Instantiate(Resources.Load("prefab_GridCube"));
+				grid[x, y].transform.localPosition = new Vector3(transform.position.x + (x * cubeXOffset), transform.position.y - (y * cubeYOffset), transform.position.z);
+				
+				//determine the GridCubes adjacencies
+				if(x != 0)
+				{
+					grid[x, y].GetComponent<GridCube>().leftNeighbour = grid[x-1, y].GetComponent<GridCube>();
+				}
+				
+				/*
+				if(x != (gridWidth - 1))
+				{
+					grid[x, y].GetComponent<GridCube>().rightNeighbour = grid[x+1, y].GetComponent<GridCube>();
+				}
+				*/
+				
+				if(y != 0)
+				{
+					grid[x, y].GetComponent<GridCube>().topNeighbour = grid[x, y-1].GetComponent<GridCube>();
+				}
+				
+				/*
+				if(y != (gridHeight - 1))
+				{
+					grid[x, y].GetComponent<GridCube>().bottomNeighbour = grid[x, y+1].GetComponent<GridCube>();
+				}
+				*/
+			}
+		}
+	}
+	
+	void Update()
+	{
+	
+	}
+}
