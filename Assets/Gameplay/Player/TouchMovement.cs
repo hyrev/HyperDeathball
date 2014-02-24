140a0d74e18dd6a0cf5ccbe26d7cbb8fddf6603e
ririari <ririari@gmail.com>
2014-02-24 17:22:09 -0400

    Redid the touch controls for the paddle
    The multi-touch movement was kinda funky so i decided to modify it once
    again, sorry about that, took me a while, but it looks a little bit more
    solid

---
+++ Assets/Gameplay/Player/TouchMovement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchMovement : MonoBehaviour {
+	public Transform paddle;
+	private bool touching;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!Camera.main.GetComponent<PauseMenu>().isPaused){
+			if(Input.GetMouseButton(0)){
+				RaycastHit hit = new RaycastHit();
+				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+				if (Physics.Raycast(ray,out hit, 100)){
+					if(hit.transform == paddle){
+						touching = true;
+					}
+				}
+			}
+			else{
+				touching = false;
+			}
+
+			if(touching){
+				Vector3 curScreenPoint = new Vector3(paddle.position.x,Input.mousePosition.y,Camera.main.transform.position.z*-1);
+				Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
+				transform.position = new Vector3(paddle.position.x,curPosition.y,0);
+
+				if(Input.GetAxis("Mouse X")<-0.2){
+					//Code for action on mouse moving left
+					transform.Rotate(new Vector3(0,Input.GetAxis("Mouse X")*10,0));
+					print("Mouse moved left");
+				}
+				if(Input.GetAxis("Mouse X")>0.2){
+					//Code for action on mouse moving right
+					transform.Rotate(new Vector3(0,Input.GetAxis("Mouse X")*10,0));
+					print("Mouse moved right");
+				}
+			}
+		}
+	}
+}
