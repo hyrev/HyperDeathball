62f1a351cf9827deb82a169de0ad1dd27d9c3718
Brian <bdinelt@uwo.ca>
2014-04-07 18:25:07 -0400

    Multitouch isn't using mouse inputs anymore!
    Also have an android build in the project folder

--- Assets/Gameplay/Player/MultiTouchMovement.cs
+++ Assets/Gameplay/Player/MultiTouchMovement.cs
@@ -9,50 +9,43 @@ public class MultiTouchMovement : MonoBehaviour {
 	public float rightMovementMax;	//Furthest the right the paddle can move
 	public float snapHeight;
 
-	private int touchNum;					//Which finger is currently touching this input
 	private bool isPaused;
+	private bool isTouching;
 
 	void Start () {
-		touchNum = 0;
+
 	}
 
 	void Update () {
 		isPaused = Camera.main.GetComponent<PauseMenu>().isPaused;
 		if(!isPaused){
-			if (Input.GetMouseButton(0)) {
+			isTouching = false;
+			foreach(Touch touch in Input.touches) {
+				if(touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled) {
+					isTouching = true;
+					//Check to see if we're actually hitting this touch point with our input
+					Ray ray = Camera.main.ScreenPointToRay(touch.position);
+					RaycastHit hit;
+					
+					if (Physics.Raycast(ray, out hit)) {
+						if(hit.collider.name == name) {
+							Vector3 curScreenPoint = new Vector3(touch.position.x,touch.position.y,Camera.main.transform.position.z * -1);
+							curScreenPoint = Camera.main.ScreenToWorldPoint(curScreenPoint);
+							curScreenPoint.z = 0;
+							transform.position = curScreenPoint;
+							Vector3 newPos = (transform.position + otherInput.transform.position) / 2.0f;
 
-				//Check to see if we're actually hitting this touch point with our input
-				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-				RaycastHit hit;
-
-				if (Physics.Raycast(ray, out hit)) {
-					if(hit.collider.name == name) {
-						if(otherInput.GetComponent <MultiTouchMovement>().touchNum == 1){
-							touchNum = 2;
-						} else {
-							touchNum = 1;
+							if(newPos.x < rightMovementMax && newPos.x > leftMovementMax) {
+								Vector3 lookPos =  (Vector3.Cross (transform.position-newPos, Vector3.forward)).normalized;
+								lookPos.z = 0;
+								player.transform.rotation = Quaternion.LookRotation(lookPos);
+								player.transform.position = newPos;
+							}
 						}
 					}
 				}
-
-				//If we have hit the input, calculate where the paddle should be
-				if(touchNum != 0){
-					Vector3 curScreenPoint = new Vector3(Input.mousePosition.x,Input.mousePosition.y,Camera.main.transform.position.z * -1);
-					curScreenPoint = Camera.main.ScreenToWorldPoint(curScreenPoint);
-					curScreenPoint.z = 0;
-					transform.position = curScreenPoint;
-					Vector3 newPos = (transform.position + otherInput.transform.position) / 2.0f;
-
-					if(newPos.x < rightMovementMax && newPos.x > leftMovementMax) {
-						Vector3 lookPos =  (Vector3.Cross (transform.position-newPos, Vector3.forward)).normalized;
-						lookPos.z = 0;
-						player.transform.rotation = Quaternion.LookRotation(lookPos);
-						player.transform.position = newPos;
-					}
-
-				}
-			} else if(Input.GetMouseButtonUp(0)) {
-				touchNum = 0;
+			}
+			if(!isTouching) {
 				SnapBack ();
 			}
 		}
